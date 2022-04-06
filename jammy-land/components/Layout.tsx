d89b521000fbb647647c752405ed5111af04dc89
jammythedreamer <jammythedreamer@gmail.com>
2022-04-07 00:08:03 +0900

    add default components

---
+++ jammy-land/components/Layout.tsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({children}: any) => (
+  <div>
+    <Header/>
+      {children}
+    <Footer/>
+  </div>
+)
+
+export default Layout;
